21036a45ef9b6160434bacfd5e1a640b0f08c874
Primemerlinian <mwatson0518@gmail.com>
2023-01-07 01:16:13 -0500

    part 2 of lab complete

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,12 +1,19 @@
 import { skills } from '../data/skills-data.js'
+import { Skill } from '../models/skills.js'
 
 
-function index (req, res) {
-  res.render('skills/index',{
-    skills:skills
+function index(req, res) {
+  Skill.find({})
+  .then(skills => { 
+    res.render('skills/index', {
+      skills: skills,
+    })
+  })
+  .catch(error => { // If there's an error, console.log it and redirect back home!
+    console.log(error)
+    res.redirect('/')
   })
 }
-
 export {
   index
 }
